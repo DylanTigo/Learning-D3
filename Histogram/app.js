4d79a745a698b170c26b1782e863c42646dcdb0d
dylantigo <dylannoeltigomo@gmail.com>
2024-09-04 06:04:37 +0100

    -Feat (Histogam): make the histogram dynamic when whe select a parameter

--- Histogram/app.js
+++ Histogram/app.js
@@ -2,9 +2,6 @@ async function draw() {
   // Data
   const dataset = await d3.json("data.json");
 
-  const xAccessor = (d) => d.currently.humidity;
-  const yAccessor = (d) => d.length;
-
   // Dimensions
   let dimensions = {
     width: 800,
@@ -29,53 +26,69 @@ async function draw() {
       `translate(${dimensions.margins}, ${dimensions.margins})`
     );
 
-  // Scales X
-  const xScale = d3
-    .scaleLinear()
-    .domain(d3.extent(dataset, xAccessor))
-    .range([0, dimensions.ctrWidth])
-    .nice();
+  const labels = ctr.append("g").classed("bar-labels", true);
+  const axisG = ctr
+      .append("g")
+      .attr("transform", `translate(0, ${dimensions.ctrHeight})`)
 
-  // Bin
-  const bin = d3.bin().domain(xScale.domain()).value(xAccessor).thresholds(10);
-  const newData = bin(dataset);
-  console.log(newData);
+  // HistogramD Draw function
+  const histogram = (metric) => {
+    const xAccessor = (d) => d.currently[metric];
+    const yAccessor = (d) => d.length;
 
-  // Scales Y
-  const yScale = d3
-    .scaleLinear()
-    .domain([0, d3.max(newData, yAccessor)])
-    .range([dimensions.ctrHeight, 0])
-    .nice();
+    // Scales
+    const xScale = d3
+      .scaleLinear()
+      .domain(d3.extent(dataset, xAccessor))
+      .range([0, dimensions.ctrWidth])
+      .nice();
 
-  ctr
-    .append("g")
-    .classed("bar-labels", true)
-    .selectAll("text")
-    .data(newData)
-    .join("text")
-    .attr("x", (d) => xScale(d.x0) + (xScale(d.x1) - xScale(d.x0)) / 2)
-    .attr("y", (d) => yScale(yAccessor(d) + 5))
-    .text(yAccessor);
+    const bin = d3
+      .bin()
+      .domain(xScale.domain())
+      .value(xAccessor)
+      .thresholds(10);
+    const newData = bin(dataset);
 
-  // Axes
-  const xAxis = d3.axisBottom(xScale);
-  const xAxisG = ctr
-    .append("g")
-    .attr("transform", `translate(0, ${dimensions.ctrHeight})`)
-    .call(xAxis);
+    const yScale = d3
+      .scaleLinear()
+      .domain([0, d3.max(newData, yAccessor)])
+      .range([dimensions.ctrHeight, 0])
+      .nice();
 
-  // Draw the bars
-  ctr
-    .selectAll("rect")
-    .data(newData)
-    .join("rect")
-    .attr("width", (d) => d3.max([xScale(d.x1) - xScale(d.x0) - 1, 0]))
-    .attr("height", (d) => dimensions.ctrHeight - yScale(yAccessor(d)))
-    .attr("x", (d) => xScale(d.x0))
-    .attr("y", (d) => yScale(yAccessor(d)))
-    .attr("fill", "purple")
-    .attr("shape-rendering", "auto");
+    // Draw the bars
+    ctr
+      .selectAll("rect")
+      .data(newData)
+      .join("rect")
+      .attr("width", (d) => d3.max([xScale(d.x1) - xScale(d.x0) - 1, 0]))
+      .attr("height", (d) => dimensions.ctrHeight - yScale(yAccessor(d)))
+      .attr("x", (d) => xScale(d.x0))
+      .attr("y", (d) => yScale(yAccessor(d)))
+      .attr("fill", "purple")
+      .attr("shape-rendering", "auto");
+
+    labels
+      .selectAll("text")
+      .data(newData)
+      .join("text")
+      .attr("x", (d) => xScale(d.x0) + (xScale(d.x1) - xScale(d.x0)) / 2)
+      .attr("y", (d) => yScale(yAccessor(d) + 5))
+      .text(yAccessor);
+
+    // Axes
+    const axis = d3.axisBottom(xScale);
+    
+      axisG.call(axis);
+  };
+
+  d3.select("#metric").on("change", (e) => {
+    e.preventDefault();
+
+    histogram(e.target.value);
+  });
+
+  histogram("humidity");
 }
 
 draw();
